f8222256a840702fe939129f66acd98f8e6669e7
Chaimalov <89201673+Chaimalov@users.noreply.github.com>
2026-09-25 06:12:55 -0700

    style: set Hebrew locale and right-to-left layout

--- App/ProbablyGroceriesApp.swift
+++ App/ProbablyGroceriesApp.swift
@@ -5,6 +5,8 @@ struct ProbablyGroceriesApp: App {
     var body: some Scene {
         WindowGroup {
             ShoppingView()
+                .environment(\.locale, Locale(identifier: "he_IL"))
+                .environment(\.layoutDirection, .rightToLeft)
         }
     }
 }
